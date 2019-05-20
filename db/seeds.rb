f8e1ced8b9a89b9c2a9a8ca49a7958961bde96eb
fabien rizzoli <fabien.rizzoli@gmail.com>
2019-05-20 13:19:37 +0200

    2nd

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Obermama = Restaurant.new(name: "Obermama", category: "italian", address: "75011 Paris")
+
+bristol = Restaurant.new(name: "Epicure", category: "french", address: "75008 Paris")
+
+Comy = Restaurant.new(name: "Co_my", category: "japanese", address: "75011 Paris")
+Popine = Restaurant.new(name: "Popine", category: "italian", address: "75011 Paris")
+Starvin = Restaurant.new(name: "Starvin_Joe", category: "belgian", address: "75011 Paris")
+
